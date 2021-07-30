96df502eb4de0d1b2e7118714ccfa6894df753b4
tish9an <tish9an@users.noreply.github.com>
2021-07-30 21:52:32 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,117 @@
+var bow , arrow,  background;
+var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+var score = 0;
+
+
+function preload(){
+  
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  blue_balloonImage = loadImage("blue_balloon0.png");
+  green_balloonImage = loadImage("green_balloon0.png");
+  pink_balloonImage = loadImage("pink_balloon0.png");
+  
+
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  //creating background
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2.5
+  
+  // creating bow to shoot arrow
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
+  
+}
+
+function draw() {
+ background(180);
+ text("score: " + score, 170,150);
+score = score + Math.round(frameCount/60);
+  
+
+// moving ground
+    scene.velocityX = -3 
+
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+  
+  //moving bow
+  bow.y = World.mouseY
+  
+   // release arrow when space key is pressed
+  if (keyDown("space")) {
+    createArrow();
+    
+  }
+  
+  //creating continous balloons
+  //var select_balloon = Math.round(random(1,2));
+  
+  //if (World.frameCount % 100 == 0) {
+    //if (select_balloon == 1) {
+      //redBalloon();
+     
+   // }
+  //}
+
+  
+      spawnBalloon();
+      drawSprites();
+}
+
+
+// Creating  arrows for bow
+ function createArrow() {
+  var arrow= createSprite(100, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
+}
+
+
+function spawnBalloon() {
+
+  if (World.frameCount % 100 === 0) {
+    balloon = createSprite(100,300,10,40);
+    balloon.velocityX = 3;
+    balloon.y = Math.round(random(100,350));
+    
+
+    var rand = Math.round(random(1,4));
+    switch(rand) {
+      case 1: 
+      balloon.addImage(red_balloonImage);
+      break;
+      case 2: 
+      balloon.addImage(green_balloonImage);
+      break;
+      case 3: 
+      balloon.addImage(blue_balloonImage);
+      break;
+      case 4: 
+      balloon.addImage(pink_balloonImage);
+      break;
+      default:
+      break;
+      
+    }
+
+    balloon.scale = 0.15
+    balloon.lifetime = 100
+    
+  }
+}
